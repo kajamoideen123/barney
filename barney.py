acc3eb16ef64360fe8177a647dde985ea769cd40
kajamoideen123 <56641143+kajamoideen123@users.noreply.github.com>
2020-05-03 01:05:35 +0530

    barney.py

--- barney.py
+++ barney.py
@@ -2,19 +2,19 @@ isSad = True
 
 class SadBarney:
 	def stop(self):
-		print "I stop being sad"
+		print("I stop being sad") 
 
 def sad():
     global isSad
     if (isSad):
         isSad = False
-        print "When I get sad,"
+        print("When I get sad,") 
         return True
     else:
         return SadBarney()
 
 def beAwesome():
-    print "and be awesome instead"
+    print("and be awesome instead") 
     
 #life motto
 if (sad() == True): 
